00a9b0b5de30d7d1ecfcb301143f0141ea6e691b
Mo <mossaab.khayrany@gmail.com>
2024-12-31 23:51:55 +0100

    first commit

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,28 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+import picbg from "/picbg.jpg"; 
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src={picbg} // Use the imported image
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Mo Khy</h3>
+					<p className='text-gray-400'>mo.khy@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+
+export default Profile;
